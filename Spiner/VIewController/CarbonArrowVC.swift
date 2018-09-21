3c5d5e01b50fcbd2cc79c40fe04db0c8f93b2938
Levente Máthé <info@leventemathe.com>
2018-09-21 11:55:21 +0200

    Finish CarbonArrowVC

--- Spiner/VIewController/CarbonArrowVC.swift
+++ Spiner/VIewController/CarbonArrowVC.swift
@@ -18,6 +18,30 @@ class CarbonArrowVC: UIViewController {
         super.viewDidLoad()
         let spine = calc.calculate(input.drawWeight, drawLength: input.drawLength)
         let arrowVC = children.first! as! ArrowVC
-        arrowVC.setSpine(String(spine))
+        let spineWithExtra = calculateSpineWithExtra(spine)
+        print("Spine: \(spine)")
+        print("Spine with extra \(spineWithExtra)")
+        arrowVC.setSpine(spineWithExtra.0)
+        arrowVC.setExtra(spineWithExtra.1)
+        arrowVC.setStats(getBowStats())
+    }
+    
+    private func calculateSpineWithExtra(_ spine: Double) -> (String, String) {
+        let spine: Int = Int(round(spine))
+        let remainder = spine % 100
+        var resultSpine = spine - remainder
+        
+        if remainder > 50 {
+            resultSpine += 100
+            return (String(resultSpine), "With heavier point or longer shaft")
+        } else if remainder == 0{
+            return (String(resultSpine), "")
+        } else {
+            return (String(resultSpine), "With lighter point")
+        }
+    }
+    
+    private func getBowStats() -> String {
+        return "\(input.drawWeight)# @ \(input.drawLength)\""
     }
 }
